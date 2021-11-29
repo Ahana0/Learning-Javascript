87c9c6221c67d503bda9202c3943e86ccfc1354d
Tanjela Ahana <ahanatanjela@gmail.com>
2021-11-29 21:02:38 +0600

    Add files via upload
    JS Fundamentals

---
+++ Javascript Fundamentals/Lexical Structure/index.js
@@ -0,0 +1,35 @@
+// To print anything is js
+console.log('Im a good girl');
+
+//To use as a calculator
+console.log(45+87);
+
+//boolean printing
+console.log(true);
+
+//array printing
+console.log([11,22,33,44]);
+
+//creating obejects
+console.log({Ahana:'this',Marks:71});
+
+//creating object table
+console.table({Ahana:'this',Marks:71});
+
+//giving warning
+console.warn('This is huge warning');
+
+//how much time does it takes to execute code
+console.time('Your code took');
+
+var name='Ahana';
+var age='22';
+var love='Ferdie';
+console.log(`${name} loves ${love}`);
+
+
+console.timeEnd('Your code took these much time');
+
+//giving error
+console.assert(453<139, 'age>139 is not possible');
+console.error('This is an error');
