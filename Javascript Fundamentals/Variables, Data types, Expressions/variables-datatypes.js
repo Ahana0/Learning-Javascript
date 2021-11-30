76c4d0a91da602a0ace95c0f360bbdcd015e98dc
Tanjela Ahana <ahanatanjela@gmail.com>
2021-11-30 22:54:21 +0600

    Update variables-datatypes.js

--- Javascript Fundamentals/Variables, Data types, Expressions/variables-datatypes.js
+++ Javascript Fundamentals/Variables, Data types, Expressions/variables-datatypes.js
@@ -38,3 +38,48 @@ camelCase => myName **Recommended**
 kebab-case => my-name
 snake_case => my_name
 PascalCase => MyName */
+
+// DATA TYPE 
+// 1. Premitive datatype --> Memory allocation in stack ex. String, Number, boolean, Null, undefined, symbol
+// 2. Reference datatype --> Memory allocation in heap ex. arrays, object literals,functions, date
+                  /* ===========================
+                      PREMETIVE DATATYPE
+                   =========================== */
+// STRING
+let university = 'Aust';
+console.log('I study at ' + university);
+console.log('The datatype is ' + (typeof university)); //type of the variable
+//NUMBER
+let credit = 81;
+console.log('I have completed '+ credit + 'credit');
+//boolean
+let bool = true;
+console.log('the data type of bool is ' +(typeof bool));
+//null
+let zero = null;
+console.log('the data type of zero is ' +(typeof zero));
+//undefined
+let undef = undefined;
+console.log('the data type of undef is ' +(typeof undef));
+
+                   /*===========================
+                      REFERENCE DATATYPE
+                   =========================== */
+//ARRAY
+let list =[23,34,465,6,65,54];
+console.log(list);
+//OBJECT LITERALS
+let marks ={
+ Ahana:71,
+ Ema:75,
+ Mehnaj:80
+};
+console.log(marks);
+//FUNCTIONS
+function findNames() {
+    
+}
+console.log(typeof findNames);
+//DATE
+let date = new Date();
+console.log(typeof date);
