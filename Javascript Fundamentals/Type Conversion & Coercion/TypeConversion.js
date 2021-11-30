f3eae03a6b82f73afe91c7fc4bf1c1086f5720da
Tanjela Ahana <ahanatanjela@gmail.com>
2021-11-30 23:25:44 +0600

    Add files via upload

---
+++ Javascript Fundamentals/Type Conversion & Coercion/TypeConversion.js
@@ -0,0 +1,35 @@
+console.log("Welcome Dear");
+
+// TYPE OF VARIABLES
+let myVar = 23;
+console.log(myVar,(typeof myVar));
+
+//CONVERSION
+let myVar1= String(43);
+console.log(myVar1,(typeof(myVar1)));
+
+let booleanVar = String(true);
+console.log(booleanVar,(typeof booleanVar));
+
+//length of an array
+let array = [1,2,3,4,5,6,7];
+console.log(array.length,(typeof array));
+
+//converting into string .toString
+let i = 34;
+console.log(i.toString());
+
+//string to number
+let num = Number('74');
+console.log(num,(typeof num));
+
+//parseint
+let num1 = parseInt(87.009);
+console.log(num1);
+
+//parseFloat
+let num2 = parseFloat(53.0987);
+console.log(num2.toFixed(8)); //toFixed is for fixing values after point
+
+
+
